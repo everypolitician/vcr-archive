b6555bc72b13cb8bce039448c764cc8d4ade72dd
Chris Mytton <chrismytton@gmail.com>
2016-07-27 20:36:05 +0100

    Add tests for the Persister

--- lib/vcr/archive.rb
+++ lib/vcr/archive.rb
@@ -1,4 +1,5 @@
 require 'vcr'
+require 'yaml'
 
 require 'vcr/archive/version'
 
@@ -28,16 +29,61 @@ module VCR
     module Persister
       extend self
 
-      def [](_)
-        nil
+      attr_reader :storage_location
+
+      def storage_location=(dir)
+        FileUtils.mkdir_p(dir) if dir
+        @storage_location = dir ? absolute_path_for(dir) : nil
       end
 
-      def []=(_, _)
-        nil
+      def [](file_name)
+        path = absolute_path_to_file(file_name)
+        files = Dir.glob("#{path}/**/*.yml")
+        return nil unless files.any?
+        interactions = files.map do |f|
+          meta = YAML.load_file(f)
+          body = File.binread(f.sub(/\.yml$/, '.html'))
+          meta['response']['body']['string'] = body
+          meta
+        end
+        {
+          'http_interactions' => interactions,
+        }
       end
 
-      def absolute_path_to_file(storage_key)
-        storage_key
+      def []=(file_name, meta)
+        path = absolute_path_to_file(file_name)
+        meta['http_interactions'].each do |interaction|
+          uri = URI.parse(interaction['request']['uri'])
+          path = File.join(path, uri.host, Digest::SHA1.hexdigest(uri.to_s))
+          directory = File.dirname(path)
+          FileUtils.mkdir_p(directory) unless File.exist?(directory)
+          body = interaction['response']['body'].delete('string')
+          File.binwrite("#{path}.yml", YAML.dump(interaction))
+          File.binwrite("#{path}.html", body)
+        end
+      end
+
+      def absolute_path_to_file(file_name)
+        return nil unless storage_location
+        File.join(storage_location, sanitized_file_name_from(file_name))
+      end
+
+      private
+
+      def absolute_path_for(path)
+        Dir.chdir(path) { Dir.pwd }
+      end
+
+      def sanitized_file_name_from(file_name)
+        parts = file_name.to_s.split('.')
+
+        # Get rid of the unneeded extension on the file_name
+        if parts.size > 1 && !parts.last.include?(File::SEPARATOR)
+          parts.pop
+        end
+
+        parts.join('.').gsub(/[^\w\-\/]+/, '_')
       end
     end
 
--- test/vcr/archive_test.rb
+++ test/vcr/archive_test.rb
@@ -17,4 +17,69 @@ describe VCR::Archive do
       assert_equal({ foo: :bar }, subject.deserialize(foo: :bar))
     end
   end
+
+  describe 'Persister' do
+    subject { VCR::Archive::Persister }
+
+    let(:tmpdir) { Dir.mktmpdir }
+    let(:uri) { 'http://example.org' }
+    let(:meta) do
+      {
+        'http_interactions' => [
+          {
+            'request' => {
+              'uri' => uri,
+            },
+            'response' => {
+              'body' => {
+                'string' => 'Hello, world.',
+              },
+            },
+          },
+        ],
+      }
+    end
+    let(:path) { File.join(tmpdir, 'foo', 'example.org', Digest::SHA1.hexdigest(uri)) }
+
+    before { subject.storage_location = tmpdir }
+
+    describe '#[]' do
+      let(:path) { subject.storage_location + '/foo/example.com/123' }
+      let(:yaml_path) { path + '.yml' }
+      let(:html_path) { path + '.html' }
+
+      before { FileUtils.mkdir_p(File.dirname(path)) }
+
+      it 'reads from the given file, relative to the configured storage location' do
+        File.write(yaml_path, YAML.dump(meta['http_interactions'].first))
+        html = '<p>Hello, world.</p>'
+        File.write(html_path, html)
+        meta['http_interactions'].first['response']['body']['string'] = html
+        assert_equal meta, subject['foo']
+      end
+
+      it 'returns nil if the directory does not exist' do
+        FileUtils.rm_rf(File.dirname(path))
+        assert_nil subject['bar']
+      end
+
+      it 'returns nil if the directory exists but is empty' do
+        FileUtils.mkdir_p(File.dirname(path))
+        assert_nil subject['foo']
+      end
+    end
+
+    describe '#[]=' do
+      it 'writes out response body to an html file' do
+        subject['foo'] = meta
+        assert_equal 'Hello, world.', File.read("#{path}.html")
+      end
+
+      it 'writes out the metadata to a yaml file' do
+        subject['foo'] = meta
+        expected = { 'request'=> { 'uri' => 'http://example.org' }, 'response' => { 'body' => {} } }
+        assert_equal expected, YAML.load_file("#{path}.yml")
+      end
+    end
+  end
 end

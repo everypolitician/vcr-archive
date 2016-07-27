bbb08235101d5b36778544057dac5f8b9ff2c039
Chris Mytton <chrismytton@gmail.com>
2016-07-27 12:23:24 +0100

    Take a git url as the cassette name

--- lib/vcr/archive.rb
+++ lib/vcr/archive.rb
@@ -6,7 +6,7 @@ module VCR
   module Archive
     class YamlSeparateHtmlSerializer
       def self.file_extension
-        'archive'
+        'git'
       end
 
       def self.serialize(hash)
@@ -18,72 +18,20 @@ module VCR
       end
     end
 
-    module YamlSeparateHtmlPersister
-      extend self
+    class GitRepository
+      attr_reader :url, :directory
 
-      def [](git_repository)
-        puts "Read from persister"
-        # VCR adds the extension from the serializer, so we need to remove it.
-        git_repository.sub!(/\.archive$/, '')
-        clone_repo_if_missing!(git_repository)
-        Dir.chdir(git_repository)
+      def initialize(git_repository_url)
+        @url = git_repository_url
+        @directory = Dir.mktmpdir
+        clone_repo_if_missing!
+        Dir.chdir(directory)
         create_or_checkout_archive_branch!
-        return nil unless File.directory?(git_repository)
-        files = Dir.glob("#{git_repository}/**/*.yml")
-        return nil if files.empty?
-        interactions = files.map do |f|
-          meta = YAML.load_file(f)
-          body = File.binread(f.sub(/\.yml$/, '.html'))
-          meta['response']['body']['string'] = body
-          meta
-        end
-        {
-          'http_interactions' => interactions,
-          'recorded_with' => File.binread(File.join(git_repository, 'recorded_with.txt')),
-        }
       end
 
-      def []=(git_repository, meta)
-        # VCR adds the extension from the serializer, so we need to remove it.
-        git_repository.sub!(/\.archive$/, '')
-        clone_repo_if_missing!(git_repository)
-        Dir.chdir(git_repository)
-        create_or_checkout_archive_branch!
-        meta['http_interactions'].each do |interaction|
-          uri = URI.parse(interaction['request']['uri'])
-          path = File.join(git_repository, uri.host, Digest::SHA1.hexdigest(uri.to_s))
-          directory = File.dirname(path)
-          FileUtils.mkdir_p(directory) unless File.exist?(directory)
-          body = interaction['response']['body'].delete('string')
-          File.binwrite("#{path}.yml", YAML.dump(interaction))
-          File.binwrite("#{path}.html", body)
-          message = "#{interaction['response']['status'].values_at('code', 'message').join(' ')} #{interaction['request']['uri']}"
-          system("git add .")
-          system("git commit --allow-empty --message='#{message}'")
-          # TODO: Use VCR hooks to run this when the cassette is ejected.
-        end
-        File.binwrite(File.join(git_repository, 'recorded_with.txt'), meta['recorded_with'])
-        system("git add .")
-        unless system("git diff --exit-code")
-          system("git commit --message='#{message}'")
-        end
-        system("git push --quiet origin #{branch_name}")
-      end
-
-      def absolute_path_to_file(storage_key)
-        storage_key
-      end
-
-      def clone_repo_if_missing!(git_repository)
-        if github_repo_url.nil?
-          warn "Could not determine git repo for 'scraped_page_archive' to use.\n\n" \
-            "See https://github.com/everypolitician/scraped_page_archive#usage for details."
-          return
-        end
-        unless File.directory?(git_repository)
-          warn "Cloning archive repo into #{git_repository}"
-          system("git clone #{git_url} #{git_repository}")
-        end
+      def clone_repo_if_missing!
+        return if File.directory?(File.join(directory, '.git'))
+        system("git clone #{url} #{directory}")
       end
 
       def create_or_checkout_archive_branch!
@@ -95,29 +43,52 @@ module VCR
         end
       end
 
-      def git_url
-        url = URI.parse(github_repo_url)
-        url.password = ENV['SCRAPED_PAGE_ARCHIVE_GITHUB_TOKEN']
-        url.to_s
-      end
-
-      def github_repo_url
-        @github_repo_url ||= (git_remote_get_url_origin || ENV['MORPH_SCRAPER_CACHE_GITHUB_REPO_URL'])
-      end
-
-      def git_remote_get_url_origin
-        remote_url = `git remote get-url origin`.chomp
-        remote_url.empty? ? nil : remote_url
-      end
-
-      # TODO: This should be configurable
-      def refresh_cache?
-        true
-      end
-
       # TODO: This should be configurable
       def branch_name
         @branch_name ||= 'scraped-pages-archive'
+      end
+    end
+
+    module YamlSeparateHtmlPersister
+      extend self
+
+      def [](git_repository)
+        # VCR adds the extension from the serializer, so we need to remove it.
+        repo = GitRepository.new(git_repository.sub!(/\.git$/, ''))
+        files = Dir.glob("#{repo.directory}/**/*.yml")
+        return nil if files.empty?
+        interactions = files.map do |f|
+          meta = YAML.load_file(f)
+          body = File.binread(f.sub(/\.yml$/, '.html'))
+          meta['response']['body']['string'] = body
+          meta
+        end
+        {
+          'http_interactions' => interactions,
+        }
+      end
+
+      def []=(git_repository, meta)
+        # VCR adds the extension from the serializer, so we need to remove it.
+        repo = GitRepository.new(git_repository.sub!(/\.git$/, ''))
+        meta['http_interactions'].each do |interaction|
+          uri = URI.parse(interaction['request']['uri'])
+          path = File.join(repo.directory, uri.host, Digest::SHA1.hexdigest(uri.to_s))
+          directory = File.dirname(path)
+          FileUtils.mkdir_p(directory) unless File.exist?(directory)
+          body = interaction['response']['body'].delete('string')
+          File.binwrite("#{path}.yml", YAML.dump(interaction))
+          File.binwrite("#{path}.html", body)
+          message = "#{interaction['response']['status'].values_at('code', 'message').join(' ')} #{interaction['request']['uri']}"
+          system("git add .")
+          system("git commit --allow-empty --message='#{message}'")
+          # TODO: Use VCR hooks to run this when the cassette is ejected.
+        end
+        system("git push --quiet origin #{repo.branch_name}")
+      end
+
+      def absolute_path_to_file(storage_key)
+        storage_key
       end
     end
 
